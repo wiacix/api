343dbd9e86db1a5cf285ebd46fbeb880e8dac0c3
Kamil Wiącek <wiacekk@logicsynergy.corp>
2023-12-31 13:43:32 +0100

    test

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post("/addEmployee", async (req, res) => {
 app.get("/employees", async (req, res) => {
     try {
       const employees = await Employee.find();
-      res.status(200).json(employees);
+      res.status(200).json('test123');
     } catch (error) {
       res.status(500).json({ message: "Failed to retrieve the employees" });
     }
